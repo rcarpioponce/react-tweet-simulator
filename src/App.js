9ec15ab1ef5b16896bcc8ce880ff048de3f37956
Renzo Carpio <me@renzocarpio.com>
2019-12-17 14:49:18 -0500

    feature: se agrego el boton SendTweet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   SnackBar
 } from '@material-ui/core';
 import Header from "./components/Header";
+import SendTweet from "./components/SendTweet";
 
 function App() {
   return ( <
@@ -13,6 +14,9 @@ function App() {
     } >
     <
     Header / >
+    >
+    <
+    SendTweet / >
     <
     /Container>
   );
